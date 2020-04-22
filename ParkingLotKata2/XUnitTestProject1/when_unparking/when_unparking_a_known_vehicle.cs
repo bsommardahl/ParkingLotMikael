ffacab774cc7d56f931ba1d31bdb0ff6a3ba1414
Mikael Maaninka <mikael.maaninka.nu>
2020-04-22 18:46:12 +0200

    Fix erro in test and some refactor

--- ParkingLotKata2/XUnitTestProject1/when_unparking/when_unparking_a_known_vehicle.cs
+++ ParkingLotKata2/XUnitTestProject1/when_unparking/when_unparking_a_known_vehicle.cs
@@ -6,7 +6,57 @@ using Xunit;
 
 namespace XUnitTestProject1.when_unparking
 {
-    public partial class when_unparking_a_known_vehicle : given_a_parking_lot
+    public class when_the_driver_do_not_have_enough_money : given_a_parking_lot
+    {
+        readonly IVehicle _vehicle;
+        readonly int _days;
+        IDriver _driver;
+        int _chargeAmount;
+        int _discountedAmount;
+        private readonly string _license;
+        private double _moneyInWallet;
+
+        public when_the_driver_do_not_have_enough_money()
+        {
+            //Arrange
+            var vehicleLength = 2;
+            _days = 1;
+
+            _license = "Valid";
+            _vehicle = A.Fake<IVehicle>();
+            _driver = A.Fake<IDriver>();
+            _chargeAmount = 10;
+            _discountedAmount = 1000;
+
+            A.CallTo(() => _vehicle.Driver).Returns(_driver);
+            A.CallTo(() => _vehicle.Length).Returns(vehicleLength);
+            A.CallTo(() => _vehicle.License).Returns(_license);
+
+            A.CallTo(() => _vehicleCostWithdrawalStrategyFactory.Create(_vehicle))
+                .Returns((vehicle, days) => _chargeAmount);
+
+            A.CallTo(() => _longTermDiscounter.Discount(_days, _chargeAmount)).Returns(_discountedAmount);
+        }
+
+
+        [Fact]
+        public void with_not_enough_money_should_throw_an_exception()
+        {
+
+            //Arrange
+            A.CallTo(() => _driver.GetWalletSum()).Returns(0);
+
+            //Act
+            Sut.ParkVehicle(_vehicle);
+            Action unparkAction = () => Sut.UnparkVehicle(_license, 1);
+
+            //Assert
+            unparkAction.Should().Throw<NotEnoughMoneyException>("The driver do not have enough money");
+
+        }
+    }
+
+    public class when_unparking_a_known_vehicle : given_a_parking_lot
     {
         readonly IVehicle _vehicle;
         readonly int _days;
